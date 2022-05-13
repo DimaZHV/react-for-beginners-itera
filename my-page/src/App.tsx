86d0b59699b1fd162510fe2151be65f93fc3289f
DimaZhuravlov <dzhv26@gmail.com>
2022-05-13 13:57:26 +0300

    Initial commit

---
+++ my-page/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+const App = () => (
+	<div className="App">
+		<h1>Dima</h1>
+		<h2>
+			27 years old. I am open to any interesting offers. I can note some of my
+			strong sides: responsibility, quick learning, perseverance in achieving
+			professional goals and focus on results. I am a team player and can build
+			and maintain good working relationships in any team. I am confident in my
+			abilities and I know that I will do my best to make our cooperation
+			successful.
+		</h2>
+		<input type="button" value="Click here!" />
+	</div>
+);
+export default App;
